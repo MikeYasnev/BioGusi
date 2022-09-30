a7674b7b16cf847b1af766bb4a9cfe958b3315f7
MikeYasnev <mikessoft@gmail.com>
2022-09-30 23:24:34 +0300

    Update project_akakin.go

--- project_akakin.go
+++ project_akakin.go
@@ -3,15 +3,18 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello LA")
+    fmt.Println("Hello LA. Project Anakin is arrived")
     fmt.Println("Prepare Stadium for 10/7/2020, invite VIP guests and people of LA")
     fmt.Println("Restore content from 2021 + updated + we with children")
-    fmt.Println("Delivery project from Moscow !)!")
-//  business jet is ok (refueling in Berlin), no visa just delivery + money for the trip
-    fmt.Println("Perform a show on 7-th")	
-// perhaps i will be alone for the show
-    fmt.Println("GO !)!")	
+    fmt.Println("Delivery project from Moscow ))")
+//  business jet is ok (refueling in Berlin), no visa just delivery + money for the trip OR yacth from SPB
+    
+    fmt.Println("Perform a show on 7-th of November")	
+    
+// I think it is better to come with my wife but she is afraid we will decide if I go first to ensure her house OR we will come together
+    fmt.Println("GO ))")	
+    
 // prepare house for my wife and children, let them come in safe
     
-    fmt.Println("one two three, zenitushka, sosi )")	
+    fmt.Println("one two three, zenitushka ))")	
 }
