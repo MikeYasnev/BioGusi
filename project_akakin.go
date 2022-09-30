dd98aaec0348d73147bb1b487baea76ae220fff5
MikeYasnev <mikessoft@gmail.com>
2022-09-30 18:54:42 +0300

    project plan
    my view

---
+++ project_akakin.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello LA")
+    fmt.Println("Prepare Stadium for 10/7/2020, invite VIP guests and people of LA")
+    fmt.Println("Restore content from 2021 + updated + we with children")
+    fmt.Println("Delivery project from Moscow !)")
+//  business jet is ok (refueling in Berlin), no visa just delivery + money for the trip
+    fmt.Println("Perform a show on 7-th")	
+// perhaps i will be alone for the show
+    fmt.Println("GO !)")	
+// prepare house for my wife and children, let them come in safe
+}
