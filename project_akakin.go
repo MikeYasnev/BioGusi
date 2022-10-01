c521a30b65af3e892ec592f350f4583b6aeb3112
MikeYasnev <mikessoft@gmail.com>
2022-10-01 12:31:56 +0300

    Update project_akakin.go

--- project_akakin.go
+++ project_akakin.go
@@ -3,20 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello LA. Project Anakin is arrived")
-    fmt.Println("Prepare Stadium for 10/7/2020, invite VIP guests and people of LA")
+    fmt.Println("Hello Roma. Project Excorcism is going")
+    fmt.Println("Prepare Roma Stadium for 10/7/2020, invite VIP guests, Pope and people of Roma")
     fmt.Println("Restore content from 2021 + updated + we with children")
     fmt.Println("Delivery project from Moscow ))")
-//  business jet is ok (refueling in Berlin), no visa just delivery + money for the trip OR yacth from SPB
     
     fmt.Println("Perform a show on 7-th of November")	
     
-// I think it is better to come with my wife but she is afraid we will decide if I go first to ensure her house OR we will come together
-    fmt.Println("GO ))")	
+    fmt.Println("GO )))")	
     
 // prepare house for my wife and children, let them come in safe
     
-    fmt.Println("one two three, zenitushka ))")	
-    
-   // i can run if necessary but doesnt see any value in it, lets build Gamma instead
+// i can run if necessary but doesnt see any value in it, lets build Gamma instead, OK lets build Gamma in Roma or we will return to Moscow after show
 }
