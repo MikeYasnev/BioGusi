6dc223e224b38ee04f2f7cc2662b5e56a5dde63c
MikeYasnev <mikessoft@gmail.com>
2022-10-04 20:42:32 +0300

    Update project_akakin.go

--- project_akakin.go
+++ project_akakin.go
@@ -6,5 +6,5 @@ func main() {
     fmt.Println("Hello Barcelona. Lets go faster and stronger!))")
     fmt.Println("Perform a show on 7-th of November, invite tatu, ask Gates, Buffet and Musk for investments")	
     
-    fmt.Println("GO )))")	
+    fmt.Println("GO !))")	
 }
