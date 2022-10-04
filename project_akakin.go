1c16f2f85286b59c05666a8128dba2a6e5960eb8
MikeYasnev <mikessoft@gmail.com>
2022-10-04 20:18:48 +0300

    Update project_akakin.go

--- project_akakin.go
+++ project_akakin.go
@@ -3,16 +3,8 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hello Roma. Project Excorcism is going")
-    fmt.Println("Prepare Roma Stadium for 10/7/2020, invite VIP guests, Pope and people of Roma")
-    fmt.Println("Restore content from 2021 + updated + we with children")
-    fmt.Println("Delivery project from Moscow ))")
-    
-    fmt.Println("Perform a show on 7-th of November")	
+    fmt.Println("Hello Barcelona. Lets go faster and stronger!))")
+    fmt.Println("Perform a show on 7-th of November, invite tatu, ask Gates, Buffet and Musk for investments")	
     
     fmt.Println("GO )))")	
-    
-// prepare house for my wife and children, let them come in safe
-    
-// i can run if necessary but doesnt see any value in it, lets build Gamma instead, OK lets build Gamma in Roma or we will return to Moscow after show
 }
